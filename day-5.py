5f9a7a2b27a183c4885b2f29cc42f301e0ec9524
Umair Imran <umairva7@gmail.com>
2025-11-04 21:59:47 +0500

    Playwright project

--- day-5.py
+++ day-5.py
@@ -1,29 +1,25 @@
 from playwright.sync_api import sync_playwright
 import time
 
-pw=sync_playwright().start()
+with sync_playwright() as p:
+    # Launch browser
+    browser = p.chromium.launch(headless=False, slow_mo=1000)
+    page = browser.new_page()
 
-#creating browser
-browser=pw.chromium.launch(headless=False,slow_mo=2000)
+    # Go to target site
+    url = "https://priceoye.pk/mobiles"
+    page.goto(url)
 
-#creating page/tab
-page=browser.new_page()
+    # Wait for product list to load
+    page.wait_for_selector("//div[@id='product_list_scroll_identifier']")
 
-url = "https://priceoye.pk/mobiles"
-page.goto(url)
+    # Find all product links
+    links = page.locator("//div[@class='productBox b-productBox']/a[@class='ga-dataset']")
 
-page=page.locator("xpath=//div[@id='product_list_scroll_identifier']")
+    # Loop through them and print href attributes
+    count = links.count()
+    for i in range(count):
+        href = links.nth(i).get_attribute("href")
+        print(href)
 
-links=page.locator("xpath=//a[@class='ga-dataset']").get_attribute("href")
-
-for link in links:
-    print(link)
-
-
-
-
-
-
-
-
-browser.close()
+    browser.close()
